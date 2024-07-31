b40710fe320785889a181c5f42f7b26465295c0e
Muhammad kholit <99956928+muhammadnorkholit@users.noreply.github.com>
2024-08-01 00:44:33 +0700

    feat: add plugin vite require

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import commonjs from "@rollup/plugin-commonjs";
+import vitePluginRequire from "vite-plugin-require";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), commonjs()],
+  plugins: [react(), commonjs(), vitePluginRequire()],
   optimizeDeps: {
     exclude: ["js-big-decimal"],
   },
