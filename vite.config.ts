c1506531940c930292874b90da87f9d03790781f
Muhammad kholit <99956928+muhammadnorkholit@users.noreply.github.com>
2024-08-01 00:41:14 +0700

    feat:add common js plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import commonjs from "@rollup/plugin-commonjs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), commonjs()],
   optimizeDeps: {
     exclude: ["js-big-decimal"],
   },
