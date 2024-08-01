be0c263e1fb50d6d6e050866011f9f041f724f26
Muhammad kholit <99956928+muhammadnorkholit@users.noreply.github.com>
2024-08-01 19:57:16 +0700

    style: update padding view note

--- src/pages/note/ViewNote.tsx
+++ src/pages/note/ViewNote.tsx
@@ -10,7 +10,7 @@ interface props {
 }
 const ViewNote: React.FC<props> = ({ note }) => {
   return (
-    <div className="p-md-4 p-2 w-100 vh-100 overflow-auto">
+    <div className="p-4  w-100 vh-100 overflow-auto">
       <div className="d-flex align-items-center justify-content-between">
         {" "}
         <h2>{note.title}</h2>
