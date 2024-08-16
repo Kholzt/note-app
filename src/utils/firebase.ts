a2a75a890976b5a0380ac752316b39a1b99dc992
Muhammad Nor kholit <99956928+muhammadnorkholit@users.noreply.github.com>
2024-08-16 21:41:03 +0700

    Update firebase.ts

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -4,15 +4,15 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getDatabase, ref, set, get, child } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyD-3KtOl97VYepNISwsPTcjOJGcf6fEZt8",
+  authDomain: "note-me-ebed3.firebaseapp.com",
+  projectId: "note-me-ebed3",
+  storageBucket: "note-me-ebed3.appspot.com",
+  messagingSenderId: "214325063875",
+  appId: "1:214325063875:web:c1d858f02cff05700bf276",
   databaseURL:
-    "",
-  measurementId: "",
+    "https://note-me-ebed3-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  measurementId: "G-H9QB326NKE",
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
